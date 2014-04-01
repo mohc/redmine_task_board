c6aa00bd639f603fb386b2e10941e682229b0cd2
Mark Olive <mark@xenon.mohc.net>
2014-04-01 15:11:06 +0100

    Add L-large, Medium-M, Small-S to task
    Conflicts:
    	app/models/task_board_column.rb

--- app/models/task_board_column.rb
+++ app/models/task_board_column.rb
@@ -27,7 +27,7 @@ class TaskBoardColumn < ActiveRecord::Base
       issues = Issue.select("issues.*, tbi.is_archived, tbi.#{order_column} as weight, tbi.issue_id") \
         .joins('LEFT OUTER JOIN task_board_issues AS tbi ON tbi.issue_id = issues.id') \
         .where("project_id IN (?) AND status_id = ? AND (is_archived IS NULL OR is_archived = FALSE)", subproject_ids, status.id) \
-        .order("weight ASC, created_on ASC")
+        .order("weight ASC, priority_id DESC, created_on ASC")
       issues.each do |issue|
         # Create a TaskBoardIssue (i.e. a Card) if one doesn't exist already.
         unless issue.issue_id
